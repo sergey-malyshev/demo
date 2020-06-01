1e550fda02af33eb42349d066e3c1ac66b1961c9
sergey <sergey.malyshev.mail@gmail.com>
2020-06-01 18:02:59 +0300

    Add Store class

---
+++ src/demo/Store.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Store {
+
+   private int price;
+   private String name;
+   
+   Store(int price, String name){
+      this.name = name;
+      this.price = price;
+   }
+}
