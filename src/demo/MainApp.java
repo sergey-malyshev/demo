6642de5e1a67d262767a6c07d9cbd57e46043e4a
sergey <sergey.malyshev.mail@gmail.com>
2020-06-01 17:53:03 +0300

    Initial commit

---
+++ src/demo/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * @author Владелец
+ *
+ */
+public class MainApp {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("Hello demo!");
+
+   }
+
+}
